bb14f0c4a71b8eb1d9791203585e71167aeea0a4
rimeger <qfarmir@gmail.com>
2023-07-17 12:25:10 +0200

    first endpoint

---
+++ MagicVilla_WebApi/Controllers/VillaAPIController.cs
@@ -0,0 +1,18 @@
+﻿using MagicVilla_WebApi.Data;
+using MagicVilla_WebApi.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_WebApi.Controllers
+{
+    //[Route("api/VillaAPI")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+    }
+}
