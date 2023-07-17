504f5ed696645ce74e3af3d40e8c9cb73574268c
rimeger <qfarmir@gmail.com>
2023-07-17 16:12:21 +0200

    first delete endpoint

--- MagicVilla_WebApi/Controllers/VillaAPIController.cs
+++ MagicVilla_WebApi/Controllers/VillaAPIController.cs
@@ -41,6 +41,15 @@ namespace MagicVilla_WebApi.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
         {
+            //if(!ModelState.IsValid)
+            //    return BadRequest(ModelState);
+
+            if(VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("", "Villa already exists!");
+                return BadRequest(ModelState);
+            }
+
             if(villaDTO == null)
                 return BadRequest();
 
@@ -51,6 +60,25 @@ namespace MagicVilla_WebApi.Controllers
             VillaStore.villaList.Add(villaDTO);
 
             return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
+        }
+
+        [HttpDelete("{id:int}", Name ="DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if(villa == null)
+                return NotFound();
+
+            VillaStore.villaList.Remove(villa);
+
+            return NoContent();
         }
     }
 }
