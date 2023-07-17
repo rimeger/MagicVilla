5cc1684c21de90ffdd9c11256ba324a7563c1ce2
rimeger <qfarmir@gmail.com>
2023-07-17 15:58:56 +0200

    first post endpoint

--- MagicVilla_WebApi/Controllers/VillaAPIController.cs
+++ MagicVilla_WebApi/Controllers/VillaAPIController.cs
@@ -16,7 +16,7 @@ namespace MagicVilla_WebApi.Controllers
             return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name ="GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -33,6 +33,24 @@ namespace MagicVilla_WebApi.Controllers
 
             return Ok(villa);
 
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
+        {
+            if(villaDTO == null)
+                return BadRequest();
+
+            if (villaDTO.Id > 0)
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villaDTO);
+
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
         }
     }
 }
