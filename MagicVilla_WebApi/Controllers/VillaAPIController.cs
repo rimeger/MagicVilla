f6140721028ea745f4c9b5873fa2ce4634c93f03
rimeger <qfarmir@gmail.com>
2023-07-17 15:46:30 +0200

    second endpoint

--- MagicVilla_WebApi/Controllers/VillaAPIController.cs
+++ MagicVilla_WebApi/Controllers/VillaAPIController.cs
@@ -7,12 +7,32 @@ namespace MagicVilla_WebApi.Controllers
     //[Route("api/VillaAPI")]
     [Route("api/[controller]")]
     [ApiController]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(200)]
+        public ActionResult<VillaDTO> GetVilla(int id) 
+        {
+            if (id == 0)
+                return BadRequest();
+
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if(villa == null)
+                return NotFound();
+
+            return Ok(villa);
+
         }
     }
 }
