8ac1a70b1d49d578d9311f3ab267b69d9c111199
rimeger <qfarmir@gmail.com>
2023-07-19 12:01:44 +0200

    automapper

---
+++ MagicVilla_WebApi/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MagicVilla_WebApi.Models;
+using MagicVilla_WebApi.Models.Dto;
+
+namespace MagicVilla_WebApi
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Villa, VillaDTO>();
+            CreateMap<VillaDTO, Villa>();
+
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+        }
+    }
+}
--- MagicVilla_WebApi/Program.cs
+++ MagicVilla_WebApi/Program.cs
@@ -1,3 +1,4 @@
+using MagicVilla_WebApi;
 using MagicVilla_WebApi.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddControllers(option => {
     //option.ReturnHttpNotAcceptable = true;
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
